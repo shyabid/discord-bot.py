d8554f32a4b86784b9eb34490ec3115e5ab69de7
empester <empester@satoru.macaroni-brill.ts.net>
2025-05-26 13:58:53 +0000

    requirements.txt update

---
+++ libs/lib/python3.12/site-packages/gender_guesser-0.4.0.dist-info/DESCRIPTION.rst
@@ -0,0 +1,77 @@
+==============
+Gender Guesser
+==============
+
+.. image:: https://travis-ci.org/lead-ratings/gender-guesser.svg?branch=master
+    :target: https://travis-ci.org/lead-ratings/gender-guesser
+
+
+This package uses the underlying data from the program "gender" by Jorg Michael (described `here <http://www.autohotkey.com/community/viewtopic.php?t=22000>`_).  Its use is pretty straightforward::
+
+    >>> import gender_guesser.detector as gender
+    >>> d = gender.Detector()
+    >>> print(d.get_gender(u"Bob"))
+    male
+    >>> print(d.get_gender(u"Sally"))
+    female
+    >>> print(d.get_gender(u"Pauley")) # should be androgynous
+    andy
+
+The result will be one of ``unknown`` (name not found), ``andy`` (androgynous), ``male``, ``female``, ``mostly_male``, or ``mostly_female``. The difference between ``andy`` and ``unknown`` is that the former is found to have the same probability to be male than to be female, while the later means that the name wasn't found in the database.
+
+I18N is fully supported::
+
+    >>> print(d.get_gender(u"\xc1lfr\xfan"))  # u"Álfrún"
+    female
+
+Additionally, you can give preference to specific countries::
+
+    >>> print(d.get_gender(u"Jamie"))
+    mostly_female
+    >>> print(d.get_gender(u"Jamie", u'great_britain'))
+    mostly_male
+
+Additionally, you can create a detector that is not case sensitive (default *is* to be case sensitive)::
+
+    >>> d = gender.Detector(case_sensitive=False)
+    >>> print(d.get_gender(u"sally"))
+    female
+    >>> print(d.get_gender(u"Sally"))
+    female
+
+Try to avoid creating many Detectors, as each creation means reading the data file.
+
+Licenses
+========
+
+The generator code is distributed under the GPLv3.  The data file nam_dict.txt is released under the GNU Free Documentation License.
+
+
+Changelog
+=========
+
+0.3.0 (2016-07-02)
+******************
+
+* Remove ``unknown_value`` init option, since it can be implemented very easily with a wrapper if needed.
+* Return ``unknown`` when name is not found and ``andy`` when it is valid equally for both male and female.
+* Test README examples as doctests.
+* Fix incorrect country-wise gender detection for non-iso886-15 names coming from line length change after data file conversion to UTF-8. See #gh2. Thanks @miquelcamprodon.
+
+
+0.2.0 (2015-12-06)
+******************
+
+* Wire in ``tox`` to test in both Python 2 and Python 3.
+* Python 2 and 3 compatiblity.
+* Remove obsolete character mapper code.
+
+For previous versions, see `sexmachine <https://github.com/ferhatelmas/sexmachine/>`_.
+
+
+Credits
+=======
+
+This is a fork of the ``SexMachine`` package by `Ferhat Elmas <https://github.com/ferhatelmas>`_. It was created to be able to publish a Python 3 compatible version to PyPI and to be able add some more improvements without bugging the original author.
+
+
---
+++ libs/lib/python3.12/site-packages/gender_guesser/detector.py
@@ -0,0 +1,101 @@
+import os.path
+import codecs
+
+
+class NoCountryError(Exception):
+    """Raised when non-supported country is queried"""
+    pass
+
+
+class Detector:
+    """Get gender by first name"""
+
+    COUNTRIES = u"""great_britain ireland usa italy malta portugal spain france
+                   belgium luxembourg the_netherlands east_frisia germany austria
+                   swiss iceland denmark norway sweden finland estonia latvia
+                   lithuania poland czech_republic slovakia hungary romania
+                   bulgaria bosniaand croatia kosovo macedonia montenegro serbia
+                   slovenia albania greece russia belarus moldova ukraine armenia
+                   azerbaijan georgia the_stans turkey arabia israel china india
+                   japan korea vietnam other_countries
+                 """.split()
+
+    def __init__(self,
+                 case_sensitive=True):
+
+        """Creates a detector parsing given data file"""
+        self.case_sensitive = case_sensitive
+        self._parse(os.path.join(os.path.dirname(__file__), "data/nam_dict.txt"))
+
+    def _parse(self, filename):
+        """Opens data file and for each line, calls _eat_name_line"""
+        self.names = {}
+        with codecs.open(filename, encoding="utf-8") as f:
+            for line in f:
+                self._eat_name_line(line.strip())
+
+    def _eat_name_line(self, line):
+        """Parses one line of data file"""
+        if line[0] not in "#=":
+            parts = line.split()
+            country_values = line[30:-1]
+            name = parts[1]
+            if not self.case_sensitive:
+                name = name.lower()
+
+            if parts[0] == "M":
+                self._set(name, u"male", country_values)
+            elif parts[0] == "1M" or parts[0] == "?M":
+                self._set(name, u"mostly_male", country_values)
+            elif parts[0] == "F":
+                self._set(name, u"female", country_values)
+            elif parts[0] == "1F" or parts[0] == "?F":
+                self._set(name, u"mostly_female", country_values)
+            elif parts[0] == "?":
+                self._set(name, u"andy", country_values)
+            else:
+                raise "Not sure what to do with a sex of %s" % parts[0]
+
+    def _set(self, name, gender, country_values):
+        """Sets gender and relevant country values for names dictionary of detector"""
+        if '+' in name:
+            for replacement in ['', ' ', '-']:
+                self._set(name.replace('+', replacement), gender, country_values)
+        else:
+            if name not in self.names:
+                self.names[name] = {}
+            self.names[name][gender] = country_values
+
+    def _most_popular_gender(self, name, counter):
+        """Finds the most popular gender for the given name counting by given counter"""
+        if name not in self.names:
+            return u"unknown"
+
+        max_count, max_tie = (0, 0)
+        best = list(self.names[name].keys())[0]
+        for gender, country_values in list(self.names[name].items()):
+            count, tie = counter(country_values)
+            if count > max_count or (count == max_count and tie > max_tie):
+                max_count, max_tie, best = count, tie, gender
+
+        return best if max_count > 0 else u"andy"
+
+    def get_gender(self, name, country=None):
+        """Returns best gender for the given name and country pair"""
+        if not self.case_sensitive:
+            name = name.lower()
+
+        if name not in self.names:
+            return u"unknown"
+        elif not country:
+            def counter(country_values):
+                country_values = list(map(ord, country_values.replace(" ", "")))
+                return (len(country_values),
+                        sum([c > 64 and c-55 or c-48 for c in country_values]))
+            return self._most_popular_gender(name, counter)
+        elif country in self.__class__.COUNTRIES:
+            index = self.__class__.COUNTRIES.index(country)
+            counter = lambda e: (ord(e[index])-32, 0)
+            return self._most_popular_gender(name, counter)
+        else:
+            raise NoCountryError("No such country: %s" % country)
