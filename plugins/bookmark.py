a71097ed09cdaf6b19101498f3a4e8d1329251e9
empester <secur208treb@gmail.com>
2025-07-10 18:08:50 +0300

    Added bookmark features

--- plugins/bookmark.py
+++ plugins/bookmark.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 import aiohttp
 
 ADD_BOOKMARK_EMOJI = "🔖"
-REMOVE_BOOKMARK_EMOJI = "❌"
+REMOVE_BOOKMARK_EMOJI = "🗑️"
 
 class Bookmark(commands.Cog):
     """Message bookmark system for personal reference"""
